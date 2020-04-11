4b72b1263728f95340479206d0de1ac249046c0f
Sebastian <sizanni@miuandes.cl>
2020-04-11 00:36:30 -0400

    Commit 2.0

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,18 +1,11 @@
 
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class Customer < ApplicationRecord
+
   has_many :orders
   has_many :tickets, through: :orders
-  validates :email, presence: true, email:true , uniqueness: true
-  validates :name , presence: true , allow_blank: false
-  validates :lastname , presence: true , allow_blank:false
+  validates :email, presence: true, uniqueness: true , format: {with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :name , :lastname ,presence: true , allow_blank: false
+
 end
 
 
